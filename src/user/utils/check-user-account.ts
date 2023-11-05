f0ee83d29a8938879183c3ec1fb15b9764781e07
Anderson Vieira <asvieira.br@gmail.com>
2023-11-05 13:51:54 -0300

    refactor: rename repository name on the dbCheckUserAccount

--- src/user/utils/check-user-account.ts
+++ src/user/utils/check-user-account.ts
@@ -2,11 +2,11 @@ import { Repository } from 'typeorm';
 import { RegisterEntity } from '../account/entities/register.entity';
 
 export async function dbCheckUserAccount(
-  registerRepository: Repository<RegisterEntity>,
+  accountRepository: Repository<RegisterEntity>,
   field: string,
   value: string,
 ) {
-  const userAccount = await registerRepository.findOne({
+  const userAccount = await accountRepository.findOne({
     where: { [field]: value },
   });
   return !!userAccount;
