f42c7b06dbb3632adf1428918dfb5995c4b4f1df
Anderson Vieira <asvieira.br@gmail.com>
2023-11-07 00:29:32 -0300

    refactor: add swagger api responses to MovieController methods

--- src/movie/movie.controller.ts
+++ src/movie/movie.controller.ts
@@ -19,7 +19,7 @@ import { RegisterMovieDto } from './dto/register-movie.dto';
 import { MovieService } from './movie.service';
 import { AuthGuard } from '../shared/guards/auth-guard';
 import { UpdateMovieDto } from './dto/update-movie.dto';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
 import { serverError } from '../shared/exceptions/server-error';
 
 @ApiTags('Movies')
@@ -29,6 +29,21 @@ export class MovieController {
 
   @UseGuards(AuthGuard)
   @Post()
+  @ApiResponse({
+    status: HttpStatus.CREATED,
+    description: 'Filme cadastrado com sucesso',
+    schema: {
+      properties: {
+        title: { type: 'string' },
+        genre: { type: 'string' },
+        releaseDate: { type: 'string', format: 'date' },
+      },
+    },
+  })
+  @ApiResponse({
+    status: HttpStatus.INTERNAL_SERVER_ERROR,
+    description: 'Houve uma falha ao cadastrar filme',
+  })
   async registerMovie(
     @Body() movieData: RegisterMovieDto,
     @Res() res: Response,
@@ -51,6 +66,22 @@ export class MovieController {
 
   @UseGuards(AuthGuard)
   @Get()
+  @ApiResponse({
+    status: HttpStatus.OK,
+    schema: {
+      type: 'object',
+      properties: {
+        id: { type: 'string' },
+        title: { type: 'string' },
+        genre: { type: 'string' },
+        releaseDate: { type: 'string', format: 'date' },
+      },
+    },
+  })
+  @ApiResponse({
+    status: HttpStatus.INTERNAL_SERVER_ERROR,
+    description: 'Houve uma falha ao carregar filmes',
+  })
   async loadMovies(
     @Query('page', new DefaultValuePipe(1), ParseIntPipe) page = 1,
     @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit = 10,
@@ -66,6 +97,18 @@ export class MovieController {
 
   @UseGuards(AuthGuard)
   @Patch('/:id')
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'Filme atualizado com sucesso',
+  })
+  @ApiResponse({
+    status: HttpStatus.NOT_FOUND,
+    description: 'Filme não encontrado',
+  })
+  @ApiResponse({
+    status: HttpStatus.INTERNAL_SERVER_ERROR,
+    description: 'Houve uma falha ao atualizar filme',
+  })
   async updateMovie(
     @Param('id') id: string,
     @Body() updateMovieData: UpdateMovieDto,
@@ -83,6 +126,18 @@ export class MovieController {
 
   @UseGuards(AuthGuard)
   @Delete('/:id')
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'Filme excluído com sucesso',
+  })
+  @ApiResponse({
+    status: HttpStatus.NOT_FOUND,
+    description: 'Filme não encontrado',
+  })
+  @ApiResponse({
+    status: HttpStatus.INTERNAL_SERVER_ERROR,
+    description: 'Houve uma falha ao excluir filme',
+  })
   async deleteMovie(@Param('id') id: string, @Res() res: Response) {
     try {
       await this.movieService.dbDeleteMovie(id);
